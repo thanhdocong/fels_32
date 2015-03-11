cac84bd6a5ed03446609c209fd87c9f219226aed
thanhdocong <docongthanh@outlook.com>
2015-03-11 08:20:39 +0700

    User Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
-module ApplicationHelper
+module ApplicationHelper 
+
+  def logged_in_user      
+    unless logged_in?        
+      store_location        
+      flash[:danger] = "Please log in."        
+      redirect_to login_url
+    end
+  end
+  
+  def logged_in_user_admin 
+    unless logged_in_admin?              
+      flash[:danger] = "Please log in."        
+      redirect_to admin_login_path
+    end
+  end
 end
